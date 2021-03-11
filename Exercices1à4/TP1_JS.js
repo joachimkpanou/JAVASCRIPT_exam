03b27042bbd8ef489ca30d6788c925e59821f3c9
joachim kpanou <ackkym34@gmail.com>
2021-03-11 19:43:49 +0100

    JAVASCRIPT_exam

--- Exercices1à4/TP1_JS.js
+++
@@ -1,8 +0,0 @@
-function get_achievement(x,y,z){
-	var sum=x+y+z;
-	if (sum>=250) return "A";
-		else if (sum>=200) return "B";
-		else if (sum>=100) return "C";
-		else return "D";
-}
-console.log(get_achievement(60,100,40));
--- Exercices1à4/TP3_JS.js
+++
@@ -1,9 +0,0 @@
-function get_sum(points){
-  var sum = 0;
-  for(var i=0; i<points.length; i++)
-    sum += points[i];
-return sum;
-  }
-
-console.log(get_sum([100,80,60,70,80]));
-  
